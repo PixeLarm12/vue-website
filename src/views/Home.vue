c785b5cf8490c73be62f5918bc6c22107d3cad98
Lucas Domingues - note <lucasrdomingues-lrd@hotmail.com>
2020-12-04 17:55:52 -0300

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <h1>👉Compre ou Venda👈</h1>
+    <ProdutosBuscar />
+    <ProdutosLista />
+  </section>
+</template>
+
+<script>
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
+
+export default {
+  name: "home",
+  components: {
+    ProdutosBuscar,
+    ProdutosLista,
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #84f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
